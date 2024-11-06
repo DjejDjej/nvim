692b451a584388483209bc16b350bbcaacb2063c
Jiri Janik <jiri.janik@firma.seznam.cz>
2024-11-06 17:36:38 +0100

    a

--- lua/settings/global.lua
+++ lua/settings/global.lua
@@ -1,33 +1,12 @@
 vim.o.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 vim.opt.fillchars = 'eob: '
-
 vim.opt.fillchars.eob = ' '
 
 vim.opt.number = true          -- Shows absolute line numbers
 vim.opt.relativenumber = true  -- Shows relative line numbers
 vim.opt.cmdheight = 0
 
-
-vim.api.nvim_set_keymap('n', 'p', ':lua ImprovedPaste()<CR>', { noremap = true, silent = true })
-
-function ImprovedPaste()
-    -- Move cursor left if not at the beginning of the line
-    if vim.fn.col('.') ~= 1 then
-        vim.api.nvim_command('normal! h')
-    end
-
-    -- Get the unnamed register content
-    local paste_content = vim.fn.getreg('"')
-    
-    -- Remove trailing newline if content is a single line
-    if string.match(paste_content, '\n$') and #vim.split(paste_content, '\n') == 2 then
-        paste_content = string.gsub(paste_content, '\n$', '')
-    end
-
-    -- Insert paste content
-    vim.api.nvim_put({paste_content}, 'c', true, true)
-end
 
 -- Enable undofile
 vim.opt.undofile = true
---
+++ lua/settings/misc.lua
@@ -0,0 +1,14 @@
+-- Define a command :Rp to replace 'string1' with 'string2'
+vim.api.nvim_create_user_command(
+    'Rp',
+    function(opts)
+        local find = opts.args:match("^(%S+)")
+        local replace = opts.args:match("%s+(.+)$")
+        if find and replace then
+            vim.cmd(":%s/" .. vim.fn.escape(find, "/") .. "/" .. vim.fn.escape(replace, "/") .. "/g")
+        else
+            print("Usage: :Rp <string1> <string2>")
+        end
+    end,
+    { nargs = 1, complete = 'custom,v:lua.custom_complete' }
+)
